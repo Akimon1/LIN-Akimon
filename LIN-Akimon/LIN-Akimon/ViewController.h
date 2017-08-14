efd24feb7001b98ad2ee3da4b9e488c5c64038b0
LIN <lin@LINdeMac.local>
2017-08-14 13:40:06 +0800

    8-13  项目初始化。

---
+++ LIN-Akimon/LIN-Akimon/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LIN-Akimon
+//
+//  Created by LIN on 2017/8/14.
+//  Copyright © 2017年 LIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LIN-Akimon/Pods/Target Support Files/Pods-LIN-Akimon/Pods-LIN-Akimon-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LIN_Akimon : NSObject
+@end
+@implementation PodsDummy_Pods_LIN_Akimon
+@end
